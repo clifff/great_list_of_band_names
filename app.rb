2fc10488cc9b01b4109b465b5bc71dc5a667bbbd
Clif Reeder <clif@sbnation.com>
2012-03-10 02:01:46 -0500

    trying to fix db stuff on heroku

--- app.rb
+++ app.rb
@@ -4,8 +4,23 @@ require 'sinatra/flash'
 require 'active_record'
 require 'uuid'
 
-dbconfig = YAML.load(File.read('config/database.yml'))
-ActiveRecord::Base.establish_connection dbconfig["#{settings.environment}"]
+if settings.environment == 'development'
+  dbconfig = YAML.load(File.read('config/database.yml'))
+  ActiveRecord::Base.establish_connection dbconfig["#{settings.environment}"]
+else
+  require 'uri'
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+
+  ActiveRecord::Base.establish_connection(
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+end
+
 
 class Idea < ActiveRecord::Base
   validates_presence_of :body
