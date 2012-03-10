53904c3c9199608dc8896d5017025d3b85833ddd
Clif Reeder <clif@sbnation.com>
2012-03-10 01:08:11 -0500

    model/controller work to save ideas

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem "redis"
 gem "rake"
 gem "activerecord"
 gem "uuid"
+gem "rack-flash", :git => 'https://github.com/ChrisCauser/rack-flash.git'
 
 group :development do
   gem "sqlite3"
--- app.rb
+++ app.rb
@@ -1,16 +1,49 @@
 require 'rubygems'
-require 'bundler'
+require 'sinatra'
+require 'rack-flash'
+require 'active_record'
+require 'uuid'
 
-Bundler.require
+dbconfig = YAML.load(File.read('config/database.yml'))
+ActiveRecord::Base.establish_connection dbconfig["#{settings.environment}"]
 
-get '/' do
+class Idea < ActiveRecord::Base
+  validates_presence_of :body
+  validates_presence_of :user_name
+end
+
+class Vote < ActiveRecord::Base
+  validates_presence_of :uuid
+  validates_presence_of :idea_id
+end
+
+enable :sessions
+use Rack::Flash
+
+before do
   unless request.cookies["uuid"]
     response.set_cookie("uuid", :value => uuid_generator.generate)
   end
+end
+
+get '/' do
+  @cookies = request.cookies
+  @ideas = Idea.all
   erb :index
 end
+
+post '/idea/new' do
+  idea = Idea.create(params['idea'])
+  if idea.valid?
+    flash[:notice] = "Idea saved!"
+    idea.save!
+  else
+    flash[:error] = "Idea invalid. Please supply an idea and username!"
+  end
+  redirect '/'
+end
+
 
 def uuid_generator
   @uuid ||= UUID.new
 end
-
